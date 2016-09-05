df557e1b1d7fdffc46fe894e60c0078cbfb4adf7
Eric Hoser <hoser18@up.edu>
2016-09-04 20:23:38 -0700

    EC Finished + Comments

--- app/src/main/java/edu/up/cs301/campusmap/CampusMapImageView.java
+++ app/src/main/java/edu/up/cs301/campusmap/CampusMapImageView.java
@@ -21,7 +21,7 @@ public class CampusMapImageView extends ImageView {
     // instance variables for the current and previous tokens
     private SimpleMapToken token;
     private SimpleMapToken prevToken;
-    private ArrayList<SimpleMapToken> list = new ArrayList<SimpleMapToken>();
+    private ArrayList<SimpleMapToken> list = new ArrayList<SimpleMapToken>(); // initalize a list of tokens
 
     public void setToken(SimpleMapToken tok)
     {
@@ -30,7 +30,7 @@ public class CampusMapImageView extends ImageView {
        // prevToken = token;
         //token = tok;
 
-        list.add(tok);
+        list.add(tok); // Add the new token to the list
 
     }
 
@@ -79,7 +79,7 @@ public class CampusMapImageView extends ImageView {
 
         for (int i = 0; i < list.size(); i++)
         {
-            list.get(i).drawOn(g);
+            list.get(i).drawOn(g); // loops printing every object stored in list to the screen
         }
     }
 }
